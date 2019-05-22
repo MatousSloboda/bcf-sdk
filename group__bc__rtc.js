5ca2407d40901bce1c63a3486115e627940bc0b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-22 13:44:08 +0000

    Deploy sdk.bigclown.com to github.com/bigclownlabs/bcf-sdk.git:gh-pages

---
+++ group__bc__rtc.js
@@ -0,0 +1,17 @@
+var group__bc__rtc =
+[
+    [ "bc_rtc_t", "structbc__rtc__t.html", [
+      [ "date", "structbc__rtc__t.html#a726375d47f261ec7266c6857d4d2c323", null ],
+      [ "day", "structbc__rtc__t.html#a32a0611f2d699d5594e8ff07393a18ae", null ],
+      [ "hours", "structbc__rtc__t.html#a24cce492acb6b9678d3ef80b82897446", null ],
+      [ "minutes", "structbc__rtc__t.html#a2b4bc8d80ec4c3cf8564f96745a12cd9", null ],
+      [ "month", "structbc__rtc__t.html#a22ee1fe0179f37f5c31df1be47951756", null ],
+      [ "seconds", "structbc__rtc__t.html#ad17c305c773951479854b3f6a27d0dad", null ],
+      [ "subseconds", "structbc__rtc__t.html#a4fe8b21a9ce588b423feffbb665968de", null ],
+      [ "timestamp", "structbc__rtc__t.html#ab061c84b52ef78509fa7da02b14cf0a4", null ],
+      [ "year", "structbc__rtc__t.html#a3a34df3a36f0a5e393fe0e3622fa7724", null ]
+    ] ],
+    [ "bc_rtc_get_date_time", "group__bc__rtc.html#gab5781f8cd17a151938f256512a614af4", null ],
+    [ "bc_rtc_init", "group__bc__rtc.html#ga9f9babc3991fa3c791abf0cd57614898", null ],
+    [ "bc_rtc_set_date_time", "group__bc__rtc.html#gab4b0fce75beecc0caeb55470663b6840", null ]
+];
---
+++ search/variables_13.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['y_5faxis',['y_axis',['../structbc__lis2dh12__result__raw__t.html#a9cd627f761b27e6716a3992fccacdf9c',1,'bc_lis2dh12_result_raw_t::y_axis()'],['../structbc__lis2dh12__result__g__t.html#ab632feb3865e52c04f75a6ef3999ccc9',1,'bc_lis2dh12_result_g_t::y_axis()']]],
+  ['year',['year',['../structbc__rtc__t.html#a3a34df3a36f0a5e393fe0e3622fa7724',1,'bc_rtc_t::year()'],['../group__bc__sam__m8q.html#ga27260c7c4106afb104657a60962e7608',1,'bc_sam_m8q_time_t::year()']]]
+];
